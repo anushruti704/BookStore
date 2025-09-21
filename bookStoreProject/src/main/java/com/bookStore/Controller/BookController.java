e353258e8aab3c6cc320f50ad8843e26103d1984
B Anushruti <B Anushruti@192.168.1.11>
2025-09-21 20:55:39 +0530

    First commit - 21-09-2025

---
+++ bookStoreProject/src/main/java/com/bookStore/Controller/BookController.java
@@ -0,0 +1,28 @@
+package com.bookStore.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookStore.dto.BookDto;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+	@GetMapping
+	public ResponseEntity<List<BookDto>> getBooks(){
+		BookDto book = BookDto.builder()
+				.title("First book")
+				.build();
+		
+		List<BookDto> books = new ArrayList<>();
+		books.add(book);
+		
+		return ResponseEntity.ok(books);
+	}
+}
